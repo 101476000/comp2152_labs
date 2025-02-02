22e0a7f35c74102d6d70a64a564bfb69bd75ef5f
101476000 <101476000@georgebrown.ca>
2025-02-02 17:35:15 -0500

    labs

---
+++ week01/Lab_week01.py
@@ -0,0 +1,26 @@
+# Lab Week 1 - Questions
+# Sample Coding Questions
+# Sample Coding Questions 01 Week 01
+# First Name: Carlos
+# Last Name: Figuera
+
+Lab Week 1 - Questions
+Sample Coding Questions
+You do not need to submit the answers to these questions this week, they are just a taste of what's to
+come. You will need to submit next week's Coding Questions.
+● Comments: add a comment that says “Sample Coding Questions 01 Week 01”. Then add a
+comment with your First and Last (official) names
+● Defining Variables: Define an array variable with the following elements 1, 4, 7, 9. (Do a quick
+search online to find the syntax)
+● Order of Operations: Define 4 variables a, b, c, d and give them the values 1, 2, 3, 4. The fully
+bracketed version of
+e = a * c - b / d is
+e = (a * c) - (b / d)
+Given the following line of code, give the Fully-Bracketed Version of:
+e = a - b ** c // d + a % c
+● Formatting: Create a variable called “temperature” with the value 32.6. Then, print the following
+sentence by using the variable and the format function, with 1 line of code.“The temperature
+today is: 32.600 degrees Celsius”. Do not hard code the extra two zeros
+● Common Functions: Ask the user to input their age, and then save that input into the variable
+“userAge”. For the user input, add 22.Then print the following sentence followed by the value of
+userAge. “Now showing the shop items filtered by age: 22”. Do not hard code the number 22.
